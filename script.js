e24766579b7992f5eee068a3c4e5a4ce03713bae
MTADCT <bourakadiismail2007@gmail.com>
2026-07-22 05:10:47 +0100

    Refactor updateElo function and fetch logic

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function updateElo() {
         console.log("Starting fetch...");
 
         const response = await fetch(
-            'https://brawl-stars-elo-fetcher-production.up.railway.app/elo/${PLAYER_TAG}',
+            `https://brawl-stars-elo-fetcher-production.up.railway.app/elo/${PLAYER_TAG}`,
             { cache: "no-store" }
         );
 
